39e6ce76f111f465afb07b184af8b92e8a4b1eb3
JangoBoogaloo <jango.boogaloo@gmail.com>
2014-07-20 15:57:28 -0500

    improve make file

--- thread/main.c
+++ thread/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "console.h"
 
-int main(int argc, char*argv[]) {
+int main(int argc, char *argv[]) 
+{
 
-    if (screen_init(40, 80)) {
+    if (screen_init(40, 80)) 
+		{
         screen_refresh();
         getchar();
         screen_fini();
