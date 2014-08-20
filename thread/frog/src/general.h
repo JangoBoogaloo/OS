bc349299ae44298490b7ded253384211a3e43aa3
xguo <xguo@iders.ca>
2014-08-20 11:47:46 -0500

    try to handle memory leak problem

--- thread/frog/src/general.h
+++ thread/frog/src/general.h
@@ -9,6 +9,7 @@
 #define APP_EXIT(status) do{printf("Exit at %s:%d, status=%d\n", __FILE__, __LINE__,  status);\
    exit(status);}while(0)
 
+void *checked_malloc(const char* file, const int line, size_t size);
 #define MALLOC(size) checked_malloc(__FILE__, __LINE__, size)
 
 #endif
--- thread/frog/src/main.c
+++ thread/frog/src/main.c
@@ -4,7 +4,7 @@
 
 #define WOOD_RIGHT_MOST (SCR_WIDTH)
 #define WOOD_LEFT_MOST (0-WOOD_WIDTH)
-#define MAX_WOOD_HEAP_SIZE 10
+#define MAX_WOOD_HEAP_SIZE 3
 
 #include <stdio.h>
 #include <unistd.h>
@@ -23,12 +23,29 @@
 static pthread_mutex_t console_mutex;
 static pthread_cond_t row_create_cvs[WOOD_ROWS];
 static pthread_mutex_t row_create_mutex[WOOD_ROWS];
+static pthread_t row_threads[WOOD_ROWS];
+
+static void cleanup(void)
+{
+  /*
+  int i=0;
+	for(i=0; i<WOOD_ROWS; i++)
+	{	
+		pthread_join(row_threads[i], NULL);
+    pthread_mutex_destroy(&row_create_mutex[i]);
+    pthread_cond_destroy(&row_create_cvs[i]);
+	}
+	pthread_mutex_destroy(&console_mutex);
+  */
+
+	screen_fini();
+}
 
 static void sighandler_generic(int n)
 {
   syslog(LOG_WARNING, "PID %d got signal %d, exiting.\n", 
 				 getpid(),  n);
-	screen_fini();
+  cleanup();
   exit(n);
 }
 
@@ -61,15 +78,14 @@ static void *move_wood(void *wood_p)
 		screen_refresh();
 		pthread_mutex_unlock(&console_mutex);
 
-		pthread_mutex_lock(&row_create_mutex[wood.row]);
 		if((SCR_WIDTH/2) == wood.x)
 		{
+		  pthread_mutex_lock(&row_create_mutex[wood.row]);
 			pthread_cond_signal(&row_create_cvs[wood.row]);
+		  pthread_mutex_unlock(&row_create_mutex[wood.row]);
 		}
-		pthread_mutex_unlock(&row_create_mutex[wood.row]);
 		sleep_ticks(DISTANCE - wood.speed);
-	}
-	while(WOOD_RIGHT_MOST >= wood.x && WOOD_LEFT_MOST <= wood.x);
+	}while(WOOD_RIGHT_MOST >= wood.x && WOOD_LEFT_MOST <= wood.x);
 	pthread_exit(NULL);
 }
 
@@ -77,17 +93,39 @@ static void *run_row(void *wood_p)
 {
 	/* copy the struct completely, guarantee atomic */
 	struct wood_t wood = *(struct wood_t*)wood_p;
-	pthread_t wood_threads[MAX_WOOD_HEAP_SIZE] = {0};
-	pthread_create(&wood_threads[0], NULL, move_wood, 
-								 (void *)&wood);
-	int i=0;
+	pthread_t *wood_threads[MAX_WOOD_HEAP_SIZE];
+  int i = 0;
+  for(int i=0; i<MAX_WOOD_HEAP_SIZE; i++)
+  {
+    wood_threads[i] = NULL;
+  }
+
+  i=0;
 	do
 	{
+    if(NULL == wood_threads[i])
+    {
+      wood_threads[i] = MALLOC(sizeof(pthread_t));
+    }
+    else
+    {
+      if(0 != pthread_join(*wood_threads[i], NULL))
+      {
+        APP_EXIT(EXIT_FAILURE);
+      }
+      free(wood_threads[i]);
+      /*
+      wood_threads[i] = NULL;
+      */
+    }
 		pthread_mutex_lock(&row_create_mutex[wood.row]);
-		pthread_create(&wood_threads[i], NULL, move_wood,
+		pthread_create(wood_threads[i], NULL, move_wood,
 									 (void *)&wood);
-		pthread_cond_wait(&row_create_cvs[wood.row], 
-										  &row_create_mutex[wood.row]);
+		if(0 != pthread_cond_wait(&row_create_cvs[wood.row], 
+										          &row_create_mutex[wood.row]))
+    {
+      APP_EXIT(EXIT_FAILURE);
+    }
 		pthread_mutex_unlock(&row_create_mutex[wood.row]);
 		i = (i+1)%MAX_WOOD_HEAP_SIZE;
 	} while(true);
@@ -96,7 +134,6 @@ static void *run_row(void *wood_p)
 
 int main(void) 
 {
-	pthread_t row_threads[WOOD_ROWS] = {0};
 	struct wood_t wood[WOOD_ROWS] = {{0}};
 
 	int direct = LEFT;
@@ -130,7 +167,7 @@ int main(void)
 		{
 			wood[i].x = WOOD_LEFT_MOST;
 		}
-
+    pthread_mutex_init(&row_create_mutex[i], NULL);
 		pthread_cond_init(&row_create_cvs[i], NULL);
 		pthread_create(&row_threads[i], NULL, 
 									 run_row, (void *)&wood[i]);	
@@ -139,10 +176,8 @@ int main(void)
 	for(i=0; i<WOOD_ROWS; i++)
 	{	
 		pthread_join(row_threads[i], NULL);
-	}
+  }
 
-	pthread_mutex_destroy(&console_mutex);
-	screen_fini();
-	syslog(LOG_WARNING, "End of Processing.\n");
+  cleanup();
 	pthread_exit(NULL);
 }
